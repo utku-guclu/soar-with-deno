ce4dbb7aac06fef09a8e4e61eb6ebec90fce8618
utku-guclu <utku_guclu@hotmail.com>
2024-12-01 20:29:47 +0300

    deno

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Application, Router } from "@oak/oak";
+import checkAuth from "~crypto/auth";
+import { deleteAccount, login, register, updateAccount } from "~middleware";
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+const router = new Router();
+const authRouter = new Router();
+const app = new Application();
+const controller = new AbortController();
+
+router.get("/hello", (ctx) => {
+  ctx.response.body = "Hello, World!";
+});
+
+router.get("/close", (ctx) => {
+  controller.abort("User has invoked to close the connection");
+  ctx.response.body = "Bye!";
+});
+
+router.post("/register", register);
+router.post("/login", login);
+authRouter.put("/auth/update", updateAccount);
+authRouter.delete("/auth/delete", deleteAccount);
+
+app.use(router.allowedMethods());
+app.use(router.routes());
+app.use(authRouter.allowedMethods());
+app.use(checkAuth, authRouter.routes());
+
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  app.listen({
+    port: 5555,
+    signal: controller.signal,
+  });
 }
---
+++ middleware/mod.ts
@@ -0,0 +1,6 @@
+import deleteAccount from "./auth/deleteAccount.ts";
+import updateAccount from "./auth/updateAccount.ts";
+import login from "./login.ts";
+import register from "./register.ts";
+
+export { deleteAccount, login, register, updateAccount };
